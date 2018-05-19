e3a70d3032f6fdc8142e778e2c786ef2c21c0e5e
Hans Wei <yilangwei@yilangs-macbook-pro.local>
2018-05-19 00:17:46 -0400

    commit all first time

---
+++ MyToolBox/Controllers/WeatherDisplayCollectionViewCell.temp_caseinsensitive_rename.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  MyToolBox
+//
+//  Created by Yilang Wei on 5/18/18.
+//  Copyright © 2018 Yilang Wei. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var testLabel: UILabel!
+    
+}
